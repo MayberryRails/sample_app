eebbc432846763d3abc72f2555babeb7042cc0fd
MayberryRails <derickmayberry@att.net>
2012-02-15 21:11:34 -0600

    added user controller and link layout tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
@@ -12,4 +13,5 @@ module ApplicationHelper
   def logo
     image_tag("logo.png", :alt => "Missing Images App", :class => "round")
   end
+
 end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -20,4 +20,28 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
+
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    response.should have_selector('title', :content => "Home")
+
+    click_link "About"
+    response.should have_selector('title', :content => "about us")
+
+    click_link "Contact"
+    response.should have_selector('title', :content => "contact us")
+
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign up")
+
+    response.should have_selector('a[href="/"]>img')
+  end
 end
